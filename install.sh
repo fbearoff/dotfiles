631153aaf57618dffd22bcfc8fc5bae308d25fba
Fbearoff <fbearoff@users.noreply.github.com>
2017-07-03 01:57:55 +0200

    add zsh-syntax-highlighting

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 tmux source-file ~/.tmux.conf
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/
 
 #set aliases
 {
@@ -14,3 +15,5 @@ tmux source-file ~/.tmux.conf
     echo 'alias weather="curl http://wttr.in/philadelphia\?u"'
     echo 'alias diff="colordiff"'
 }  >> ~/.oh-my-zsh/custom/aliases.zsh
+
+source ~/.zshrc
