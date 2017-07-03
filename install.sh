99c4ab1d152e2c5aad444e92b2f783e3aa7e3cfd
frank <fbearoff@users.noreply.github.com>
2017-07-02 20:01:16 -0400

    fix zsh-syntax-highlighting directory

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 tmux source-file ~/.tmux.conf
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 #set aliases
 {
@@ -15,5 +15,3 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/
     echo 'alias weather="curl http://wttr.in/philadelphia\?u"'
     echo 'alias diff="colordiff"'
 }  >> ~/.oh-my-zsh/custom/aliases.zsh
-
-source ~/.zshrc
