345a1ef25533c888aa3dd5dc1b233ea5ebb0b7f8
Frank <fbearoff@users.noreply.github.com>
2018-03-29 11:36:32 -0400

    adapt to 1080p external monitor

--- .config/scripts/screen.sh
+++ .config/scripts/screen.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if  xrandr -q | grep "HDMI1 con";
-    then xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 1280x1024 --pos 1440x0 --rotate normal --output LVDS1 --primary --mode 1440x900 --pos 0x0 --rotate normal --output VGA1 --off
-    else echo "HDMI1 not connected"
-    fi
+if  xrandr -q | grep "DP1";
+then
+    xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --mode 1920x1080 --pos 1720x0 --rotate normal --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1440x900 --pos 0x496 --rotate normal --output VGA1 --off
+else
+    echo "DP1 not connected"
+fi
