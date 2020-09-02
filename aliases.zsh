1b69a1a44856a1348a956cf8f1341773d1237ceb
Frank <fbearoff@users.noreply.github.com>
2020-09-02 10:10:21 -0400

    update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -4,5 +4,7 @@ alias mv="mv -iv"
 alias ll="ls -alhtr"
 alias weather="curl http://wttr.in/philadelphia\?u"
 alias diff="colordiff"
-alias sync_music="rsync -v -rlt --chmod=a=rw,Da+x --delete --exclude='*.jpg' --exclude='*.ini' --progress \"/mnt/d/Music/\" \"192.168.1.102::music//\""
 alias bm="beet ls -a missing:1.. -f '$year-$albumartist-$album https://musicbrainz.org/release-group/$mb_releasegroupid $missing'"
+
+alias sm="rsync -v -rlt --chmod=a=rw,Da+x --delete --exclude='*.jpg' --exclude='*.ini' --progress \"/mnt/d/Music/\" \"192.168.1.102::music//\""
+alias bum="rsync -v -rlt --chmod=a=rw,Da+x --delete --exclude='*.jpg' --exclude='*.ini' --progress \"/mnt/d/Music/\" \"/mnt/f/Music/\""
