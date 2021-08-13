acb57e992fb58d67926c0753f5fea2efbf675e02
Frank <fbearoff@users.noreply.github.com>
2021-08-13 08:25:47 -0400

    add dunst

--- .config/scripts/screen.sh
+++ .config/scripts/screen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xrandr --output LVDS1 --primary --mode 1440x900 --rotate normal --output DP2 --mode 1920x1080 --right-of LVDS1 --rotate normal
+xrandr --output LVDS1 --mode 1440x900 --rotate normal --output DP2 --mode 1920x1080 --primary --right-of LVDS1 --rotate normal
