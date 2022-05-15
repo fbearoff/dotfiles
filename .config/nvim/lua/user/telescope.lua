b493fc748e820ca9c882a1935efb6910da6f6ca1
frank <fbearoff@users.noreply.github.com>
2022-05-15 19:42:20 -0400

    tweak telescope

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -11,6 +11,8 @@ telescope.setup {
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
+    initial_mode = "normal",
+    winblend = 5,
 
     mappings = {
       i = {
