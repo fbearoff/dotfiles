de189b7d1a683213f4dd0a4a94322d55a1747441
frank <fbearoff@users.noreply.github.com>
2022-06-07 19:03:28 -0400

    more git aliases

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,24 +1,24 @@
-alias nvimrc='nvim ~/.config/nvim/'
+alias nvimrc="nvim ~/.config/nvim/"
 
 # Colorize grep output (good for log files)
-alias grep='grep --color=auto'
-alias egrep='egrep --color=auto'
-alias fgrep='fgrep --color=auto'
+alias grep="grep --color=auto"
+alias egrep="egrep --color=auto"
+alias fgrep="fgrep --color=auto"
 
 # confirm before overwriting something
 alias cp="cp -iv"
-alias mv='mv -iv'
-alias rm='rm -iv'
+alias mv="mv -iv"
+alias rm="rm -iv"
 
 # easier to read disk
-alias df='df -h'     # human-readable sizes
-alias free='free -m' # show sizes in MB
+alias df="df -h"     # human-readable sizes
+alias free="free -m" # show sizes in MB
 
 # get top process eating memory
-alias psmem='ps auxf | sort -nr -k 4 | head -5'
+alias psmem="ps auxf | sort -nr -k 4 | head -5"
 
 # get top process eating cpu ##
-alias pscpu='ps auxf | sort -nr -k 3 | head -5'
+alias pscpu="ps auxf | sort -nr -k 3 | head -5"
 
 alias weather="curl http://wttr.in/philadelphia\?u"
 alias R="R --quiet"
@@ -38,3 +38,5 @@ alias gst="git status"
 alias ga="git add"
 alias gau="git add -u"
 alias gcm="git commit -m"
+alias gp="git push"
+alias gl="git pull"
