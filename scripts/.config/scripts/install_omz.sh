86103942cf5ba5b853f278bcfac79b0810ae789b
Frank <fbearoff@users.noreply.github.com>
2022-06-29 08:48:01 -0400

    unattended omzsh install

--- scripts/.config/scripts/install_omz.sh
+++ scripts/.config/scripts/install_omz.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ZSH="$HOME/.config/zsh/oh-my-zsh" sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ZSH="$HOME/.config/zsh/oh-my-zsh" sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 
 ZSH_CUSTOM=$HOME/.config/zsh/oh-my-zsh/custom
 
