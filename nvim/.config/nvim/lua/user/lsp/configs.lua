44034d049c21238898c74d0660c39a28b3851abc
Frank <fbearoff@users.noreply.github.com>
2022-07-05 20:33:08 -0400

    fix barbar icons

--- nvim/.config/nvim/lua/user/lsp/configs.lua
+++ nvim/.config/nvim/lua/user/lsp/configs.lua
@@ -26,7 +26,7 @@ for _, server in pairs(servers) do
   }
   local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
   if has_custom_opts then
-    opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
+    opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
   end
   lspconfig[server].setup(opts)
 end
