3d8c84e5278be3335c318ee29b85dbabad80783b
Frank <fbearoff@users.noreply.github.com>
2022-07-19 20:19:20 -0400

    new surround options

--- nvim/.config/nvim/lua/user/surround.lua
+++ nvim/.config/nvim/lua/user/surround.lua
@@ -4,9 +4,14 @@ if not status_ok then
 end
 surround.setup({
   keymaps = { -- vim-surround style keymaps
-    insert = "ys",
-    insert_line = "yss",
+    insert = "<C-g>s",
+    insert_line = "<C-g>S",
+    normal = "ys",
+    normal_cur = "yss",
+    normal_line = "yS",
+    normal_cur_line = "ySS",
     visual = "S",
+    visual_line = "gS",
     delete = "ds",
     change = "cs",
   },
