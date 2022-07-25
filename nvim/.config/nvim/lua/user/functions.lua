9eda7aeecd5fe606a604f81b882726b87a9c3589
Frank <fbearoff@users.noreply.github.com>
2022-07-24 20:02:44 -0400

    remove qf_toggle

--- nvim/.config/nvim/lua/user/functions.lua
+++ nvim/.config/nvim/lua/user/functions.lua
@@ -8,22 +8,4 @@ M.isNotEmpty = function(s)
 	return s ~= nil and s ~= ""
 end
 
--- toggle quickfixlist
-M.toggle_qf = function()
-	local windows = fn.getwininfo()
-	local qf_exists = false
-	for _, win in pairs(windows) do
-		if win["quickfix"] == 1 then
-			qf_exists = true
-		end
-	end
-	if qf_exists == true then
-		cmd.cclose()
-		return
-	end
-	if M.isNotEmpty(fn.getqflist()) then
-		cmd.copen()
-	end
-end
-
 return M
