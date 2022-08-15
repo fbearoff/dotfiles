8b1d71a29aa92d7c5088bc3294bf6f72f384a317
Frank <fbearoff@users.noreply.github.com>
2022-08-14 21:16:44 -0400

    colorizer tweak

--- nvim/.config/nvim/lua/user/colorizer.lua
+++ nvim/.config/nvim/lua/user/colorizer.lua
@@ -3,4 +3,17 @@ if not status_ok then
   return
 end
 
-colorizer.setup {}
+colorizer.setup {
+  DEFAULT_OPTIONS = {
+    RGB      = true; -- #RGB hex codes
+    RRGGBB   = true; -- #RRGGBB hex codes
+    names    = true; -- "Name" codes like Blue oe blue
+    RRGGBBAA = true; -- #RRGGBBAA hex codes
+    rgb_fn   = false; -- CSS rgb() and rgba() functions
+    hsl_fn   = false; -- CSS hsl() and hsla() functions
+    css      = false; -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+    css_fn   = false; -- Enable all CSS *functions*: rgb_fn, hsl_fn
+    -- Available modes: foreground, background, virtualtext
+    mode     = 'background'; -- Set the display mode.
+  }
+}
