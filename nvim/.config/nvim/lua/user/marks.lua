cabaed331b4cc81a6641aa12247a6f8759669cb6
Frank <fbearoff@users.noreply.github.com>
2022-12-05 20:28:58 -0500

    remove marks mappings

--- nvim/.config/nvim/lua/user/marks.lua
+++ nvim/.config/nvim/lua/user/marks.lua
@@ -4,15 +4,13 @@ if not status_ok then
 end
 
 marks.setup {
-  default_mappings = true,
+  default_mappings = false,
   builtin_marks = { ".", "<", ">", "^" },
   cyclic = true,
   refresh_interval = 250,
   sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
   excluded_filetypes = { "lspinfo", "toggleterm" },
-  bookmark_0 = {
-    sign = "⚑",
-    virt_text = "hello world"
-  },
-  mappings = {}
+  mappings = {
+    delete_line = "dm"
+  }
 }
