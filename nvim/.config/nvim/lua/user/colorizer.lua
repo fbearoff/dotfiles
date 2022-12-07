5c293074d92449b4278b3f1e90c4bafc1ca20d3c
Frank <fbearoff@users.noreply.github.com>
2022-12-07 18:37:00 -0500

    use virtual text for color indication

--- nvim/.config/nvim/lua/user/colorizer.lua
+++ nvim/.config/nvim/lua/user/colorizer.lua
@@ -4,16 +4,44 @@ if not status_ok then
 end
 
 colorizer.setup {
-  DEFAULT_OPTIONS = {
-    RGB      = true; -- #RGB hex codes
-    RRGGBB   = true; -- #RRGGBB hex codes
-    names    = true; -- "Name" codes like Blue oe blue
-    RRGGBBAA = true; -- #RRGGBBAA hex codes
-    rgb_fn   = false; -- CSS rgb() and rgba() functions
-    hsl_fn   = false; -- CSS hsl() and hsla() functions
-    css      = false; -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-    css_fn   = false; -- Enable all CSS *functions*: rgb_fn, hsl_fn
-    -- Available modes: foreground, background, virtualtext
-    mode     = 'background'; -- Set the display mode.
-  }
+  filetypes = { "*",
+    "!dirvish",
+    "!fugitive",
+    "!alpha",
+    "!NvimTree",
+    "!packer",
+    "!neogitstatus",
+    "!Trouble",
+    "!lir",
+    "!Outline",
+    "!spectre_panel",
+    "!toggleterm",
+    "!DressingSelect",
+    "!TelescopePrompt",
+  },
+  user_default_options = {
+    RGB = true, -- #RGB hex codes
+    RRGGBB = true, -- #RRGGBB hex codes
+    names = true, -- "Name" codes like Blue or blue
+    RRGGBBAA = true, -- #RRGGBBAA hex codes
+    AARRGGBB = false, -- 0xAARRGGBB hex codes
+    rgb_fn = false, -- CSS rgb() and rgba() functions
+    hsl_fn = false, -- CSS hsl() and hsla() functions
+    css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+    css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+    -- Available modes for `mode`: foreground, background,  virtualtext
+    mode = "virtualtext", -- Set the display mode.
+    -- Available methods are false / true / "normal" / "lsp" / "both"
+    -- True is same as normal
+    tailwind = false, -- Enable tailwind colors
+    -- parsers can contain values used in |user_default_options|
+    sass = { enable = false, parsers = { css }, }, -- Enable sass colors
+    virtualtext = "⬤",
+  },
+  -- all the sub-options of filetypes apply to buftypes
+  buftypes = {
+    "!nofile",
+    "!prompt",
+    "!popup",
+  },
 }
