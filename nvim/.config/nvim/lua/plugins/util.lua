440eb3a1b4428a873b82df9a68469ffcaa2dba0c
Frank <fbearoff@users.noreply.github.com>
2023-03-11 13:47:15 -0500

    add flatten

--- nvim/.config/nvim/lua/plugins/util.lua
+++ nvim/.config/nvim/lua/plugins/util.lua
@@ -43,4 +43,36 @@ return {
       { "<leader>sU", "<cmd>UrlView lazy<cr>", desc = "Plugin URLs" },
     },
   },
+
+  -- Prevent nesting of nvim instances
+  {
+    "willothy/flatten.nvim",
+    event = "VeryLazy",
+    opts = {
+      callbacks = {
+        pre_open = function()
+          require("toggleterm").toggle(0)
+        end,
+        post_open = function(bufnr, winnr, ft)
+          if ft == "gitcommit" then
+            vim.api.nvim_create_autocmd("BufWritePost", {
+              buffer = bufnr,
+              once = true,
+              callback = function()
+                vim.defer_fn(function()
+                  vim.api.nvim_buf_delete(bufnr, {})
+                end, 50)
+              end,
+            })
+          else
+            require("toggleterm").toggle(0)
+            vim.api.nvim_set_current_win(winnr)
+          end
+        end,
+        block_end = function()
+          require("toggleterm").toggle(0)
+        end,
+      },
+    },
+  },
 }
