14547d3b2699f60d644d1bf6be2640138e8ba4c0
Frank <fbearoff@users.noreply.github.com>
2023-03-25 19:45:36 -0400

    add spider

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -206,10 +206,46 @@ return {
   -- Macro recording
   {
     "chrisgrieser/nvim-recorder",
-    lazy = false,
-    config = function()
-      require("recorder").setup({})
-    end,
+    opts = {},
+  },
+
+  -- Additional Word Definitions
+  {
+    "chrisgrieser/nvim-spider",
+    keys = {
+      {
+        mode = { "n", "o", "x" },
+        "w",
+        function()
+          require("spider").motion("w")
+        end,
+        desc = "Spider-w",
+      },
+      {
+        mode = { "n", "o", "x" },
+        "e",
+        function()
+          require("spider").motion("e")
+        end,
+        desc = "Spider-e",
+      },
+      {
+        mode = { "n", "o", "x" },
+        "b",
+        function()
+          require("spider").motion("b")
+        end,
+        desc = "Spider-b",
+      },
+      {
+        mode = { "n", "o", "x" },
+        "ge",
+        function()
+          require("spider").motion("ge")
+        end,
+        desc = "Spider-ge",
+      },
+    },
   },
 
   -- Quick tagged file switching
