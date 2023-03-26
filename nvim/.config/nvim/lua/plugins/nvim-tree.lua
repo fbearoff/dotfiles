32dfc1545ed66e5411325e92e2c160212850739d
Frank <fbearoff@users.noreply.github.com>
2023-03-26 10:01:28 -0400

    remove custom rainbow queries, update obsolete nerd font symbols

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -38,7 +38,7 @@ return {
         enable = true,
         show_on_dirs = true,
         icons = {
-          hint = " ",
+          hint = "󰌶 ",
           info = " ",
           warning = " ",
           error = " ",
