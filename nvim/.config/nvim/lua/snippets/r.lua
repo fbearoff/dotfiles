2a826492c8e6d139db5ea1742af4b3cf4ab72e74
Frank <fbearoff@users.noreply.github.com>
2023-03-28 13:08:01 -0400

    add R luasnips

---
+++ nvim/.config/nvim/lua/snippets/r.lua
@@ -0,0 +1,24 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+
+ls.add_snippets(nil, {
+  r = {
+    s({
+      trig = ">>",
+      namr = "pipe",
+      dscr = "Magrittr Pipe (%>%)",
+      snippetType = "autosnippet",
+    }, {
+      t({ "%>% " }),
+    }),
+    s({
+      trig = "<<",
+      namr = "l_assign",
+      dscr = "Left Assign (<-)",
+      snippetType = "autosnippet",
+    }, {
+      t({ "<- " }),
+    }),
+  },
+})
