c83258391a01d7e4ef95067b929df34f5635102a
Frank <fbearoff@users.noreply.github.com>
2023-04-09 11:44:59 -0400

    ts-node R actions now merged

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -12,36 +12,7 @@ return {
         desc = "Trigger Node Action",
       },
     },
-    opts = function()
-      local actions = require("ts-node-action.actions")
-      local boolean_override = {
-        ["TRUE"] = "FALSE",
-        ["FALSE"] = "TRUE",
-      }
-      local operators = {
-        ["!="] = "==",
-        ["=="] = "!=",
-        [">"] = "<",
-        ["<"] = ">",
-        [">="] = "<=",
-        ["<="] = ">=",
-        ["+"] = "-",
-        ["-"] = "+",
-        ["*"] = "/",
-        ["/"] = "*",
-        ["|"] = "&",
-        ["&"] = "|",
-        ["||"] = "&&",
-        ["&&"] = "||",
-      }
-      return {
-        r = {
-          ["true"] = actions.toggle_boolean(boolean_override),
-          ["false"] = actions.toggle_boolean(boolean_override),
-          ["binary"] = actions.toggle_operator(operators),
-        },
-      }
-    end,
+    opts = {},
   },
 
   -- Show code context as top line
