4837eeb177f4b75091cf9993e6b93d42b5d1441f
Frank <fbearoff@users.noreply.github.com>
2023-04-16 08:58:12 -0400

    don't auto create dirs for urls

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -32,6 +32,9 @@ vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
 vim.api.nvim_create_autocmd("BufWritePre", {
   group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
   callback = function(event)
+    if event.match:match("^%w%w+://") then
+      return
+    end
     local file = vim.loop.fs_realpath(event.match) or event.match
 
     vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
