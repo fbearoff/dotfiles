55c3bdff8418183ecdc3618ddffd6c0cfdf2a12c
Frank <fbearoff@users.noreply.github.com>
2023-05-21 20:24:01 -0400

    R ts-node multiline actions now merged

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -13,50 +13,6 @@ return {
       },
     },
     opts = {},
-    -- opts = function()
-    --   local actions = require("ts-node-action.actions")
-    --   local padding = {
-    --     [","] = "%s ",
-    --     ["="] = " %s ",
-    --   }
-    --
-    --   local helpers = require("ts-node-action.helpers")
-    --
-    --   --- @param node TSNode
-    --   local function toggle_multiline_args(node)
-    --     local structure = helpers.destructure_node(node)
-    --     if (type(structure["arguments"]) == "table") or (type(structure["arguments"]) == "string") then
-    --     else
-    --       vim.print("No arguments")
-    --       return
-    --     end
-    --
-    --     local range_end = {}
-    --     range_end = { node:named_child(0):range() }
-    --     local replacement
-    --
-    --     if helpers.node_is_multiline(node) then
-    --       local tbl = actions.toggle_multiline(padding)
-    --       replacement = tbl[1][1](node)
-    --     else
-    --       replacement = { structure["function"] .. "(" }
-    --       for k in string.gmatch(structure.arguments, "([^,]+)") do
-    --         table.insert(replacement, k .. ",")
-    --       end
-    --       replacement[#replacement] = string.gsub(replacement[#replacement], "(.*)%,$", "%1")
-    --       table.insert(replacement, ")")
-    --     end
-    --
-    --     return replacement, { cursor = { col = range_end[4] - range_end[2] }, format = true }
-    --   end
-    --
-    --   return {
-    --     r = {
-    --       ["formal_parameters"] = actions.toggle_multiline(padding),
-    --       ["call"] = { { toggle_multiline_args, name = "Toggle Multiline Arguments" } },
-    --     },
-    --   }
-    -- end,
   },
 
   -- Show code context as top line
