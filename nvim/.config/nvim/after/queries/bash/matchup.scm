cc8853f5f014717013ddd69542088152999328e8
Frank <fbearoff@users.noreply.github.com>
2023-05-30 20:46:44 -0400

    Additional bash TS queries

--- nvim/.config/nvim/after/queries/bash/matchup.scm
+++ nvim/.config/nvim/after/queries/bash/matchup.scm
@@ -6,12 +6,23 @@
     "done" @close.while)) @scope.while
 
 (for_statement
-  "for" @open.for
+  ("for")? @open.for
+  ("select")? @open.for
   (do_group
     "done" @close.for)) @scope.for
 
+(if_statement
+  "if" @open.if
+  "fi" @close.if) @scope.if
+
+(else_clause
+  "else" @mid.if.1)
+
 (elif_clause
   "elif" @mid.if.2)
+
+((word) @mid.if.3 (#eq? @mid.if.3 "break"))
+((word) @mid.if.4 (#eq? @mid.if.4 "continue"))
 
 (case_statement
   "case" @open.case
