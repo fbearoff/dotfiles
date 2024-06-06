412138faea19eb7af82e49d17b8e89f41ba708ff
Frank <fbearoff@users.noreply.github.com>
2024-06-06 18:58:29 -0400

    update music sync command

--- zsh/.config/zsh/oh-my-zsh/custom/aliases.zsh
+++ zsh/.config/zsh/oh-my-zsh/custom/aliases.zsh
@@ -54,7 +54,7 @@ alias radian="radian --quiet"
 
 # music management
 alias bm="beet ls -a missing:1.. -f '$year-$albumartist-$album https://musicbrainz.org/release-group/$mb_releasegroupid $missing'"
-alias sm="rsync -v -rltO --chmod=a=rw,Da+x --delete --exclude='*.jpg' --exclude='*.ini' --progress \"/mnt/d/Music/\" \"omv:$HOME/pool/media/music\""
+alias sm="rsync -v -rltO --chmod=a=rw,Da+x --delete --exclude='*.jpg' --exclude='*.ini' --progress \"/mnt/d/Music/\" \"omv:$HOME/media/music\""
 alias bum="rsync -v -rlt --chmod=a=rw,Da+x --delete --exclude='*.jpg' --exclude='*.ini' --progress \"/mnt/d/Music/\" \"/mnt/e/Music/\""
 alias mi="mediainfo"
 alias gm="cd ~/downloads/music && lftp hosting -e 'cd red'"
