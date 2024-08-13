6c417473224701057059a8e4b465dc9467a735b6
Frank <fbearoff@users.noreply.github.com>
2024-08-13 09:07:41 -0400

    remove shellgpt

--- zsh/.config/zsh/oh-my-zsh/custom/functions.zsh
+++ zsh/.config/zsh/oh-my-zsh/custom/functions.zsh
@@ -65,14 +65,3 @@ function zhead() {
 function ztail() {
   zcat "$1" | tail -n 50
 }
-
-# Shell-GPT integration ZSH v0.1
-_sgpt_zsh() {
-  _sgpt_prev_cmd=$BUFFER
-  BUFFER+="⌛"
-  zle -I && zle redisplay
-  BUFFER=$(sgpt --shell <<< "$_sgpt_prev_cmd")
-  zle end-of-line
-}
-zle -N _sgpt_zsh
-bindkey ^l _sgpt_zsh
