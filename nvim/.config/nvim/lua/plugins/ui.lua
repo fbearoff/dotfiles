674b71d06ae083a49886b476e7f445f069e70bbc
Frank <fbearoff@users.noreply.github.com>
2024-12-19 12:08:13 -0500

    revert dressing changes

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -239,7 +239,18 @@ return {
   -- Fancy select and input dialogs
   {
     "stevearc/dressing.nvim",
-    opts = {},
+    init = function()
+      vim.ui.select = function(...)
+        ---@diagnostic disable-next-line: different-requires
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.select(...)
+      end
+      vim.ui.input = function(...)
+        ---@diagnostic disable-next-line: different-requires
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
+    end,
   },
 
   -- Show code context
