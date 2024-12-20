afc65eec52940d338cc33952367c82aa61f15320
Frank <fbearoff@users.noreply.github.com>
2024-12-19 20:07:27 -0500

    autoformat toggle, range formatting

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -231,12 +231,34 @@ return {
     "stevearc/conform.nvim",
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
+    init = function()
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+        require("lazy.core.util").info("Autoformat Disabled", { title = "Option" })
+      end, {
+        desc = "Disable autoformat-on-save",
+        bang = true,
+      })
+      vim.api.nvim_create_user_command("FormatEnable", function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+        require("lazy.core.util").info("Autoformat Enabled", { title = "Option" })
+      end, {
+        desc = "Re-enable autoformat-on-save",
+      })
+    end,
     keys = {
       {
         "<leader>cf",
         function()
           require("conform").format({ async = true })
         end,
+        mode = "",
         desc = "Format buffer",
       },
       {
@@ -244,17 +266,32 @@ return {
         function()
           require("conform").format({ async = true })
         end,
+        mode = "",
         desc = "Format buffer",
+      },
+      {
+        "<leader>uf",
+        function()
+          if vim.b.disable_autoformat or vim.g.disable_autoformat then
+            vim.cmd("FormatEnable")
+          else
+            vim.cmd("FormatDisable")
+          end
+        end,
+        desc = "Toggle Autoformat",
       },
     },
     opts = {
-      notify_on_error = false,
       default_format_opts = {
         lsp_format = "fallback",
       },
-      format_on_save = {
-        timeout_ms = 500,
-      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
+      end,
       formatters_by_ft = {
         lua = { "stylua" },
         sh = { "shfmt" },
