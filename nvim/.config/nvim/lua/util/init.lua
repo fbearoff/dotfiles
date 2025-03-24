c0af6961e44fc893796d0b10c54deb9608bb81de
Frank <fbearoff@users.noreply.github.com>
2025-03-24 12:19:32 -0400

    remove unreferenced util functions

--- nvim/.config/nvim/lua/util/init.lua
+++ nvim/.config/nvim/lua/util/init.lua
@@ -1,7 +1,5 @@
 local M = {}
 
-M.root_patterns = { ".git", "lua" }
-
 -- Return options
 function M.opts(name)
   local plugin = require("lazy.core.config").plugins[name]
@@ -17,26 +15,6 @@ function M.has(plugin)
   return require("lazy.core.config").spec.plugins[plugin] ~= nil
 end
 
--- Execute code on plugin load
-function M.on_load(name, fn)
-  local Config = require("lazy.core.config")
-  if Config.plugins[name] and Config.plugins[name]._.loaded then
-    vim.schedule(function()
-      fn(name)
-    end)
-  else
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "LazyLoad",
-      callback = function(event)
-        if event.data == name then
-          fn(name)
-          return true
-        end
-      end,
-    })
-  end
-end
-
 -- LSP on_attach
 function M.on_attach(on_attach)
   vim.api.nvim_create_autocmd("LspAttach", {
@@ -46,41 +24,6 @@ function M.on_attach(on_attach)
       on_attach(client, buffer)
     end,
   })
-end
-
--- returns the root directory based on:
--- * lsp workspace folders
--- * lsp root_dir
--- * root pattern of filename of the current buffer
--- * root pattern of cwd
-function M.get_root()
-  local path = vim.api.nvim_buf_get_name(0)
-  path = path ~= "" and vim.uv.fs_realpath(path) or nil
-  local roots = {}
-  if path then
-    for _, client in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
-      local workspace = client.config.workspace_folders
-      local paths = workspace and vim.tbl_map(function(ws)
-        return vim.uri_to_fname(ws.uri)
-      end, workspace) or client.config.root_dir and { client.config.root_dir } or {}
-      for _, p in ipairs(paths) do
-        local r = vim.uv.fs_realpath(p)
-        if path:find(r, 1, true) then
-          roots[#roots + 1] = r
-        end
-      end
-    end
-  end
-  table.sort(roots, function(a, b)
-    return #a > #b
-  end)
-  local root = roots[1]
-  if not root then
-    path = path and vim.fs.dirname(path) or vim.uv.cwd()
-    root = vim.fs.find(M.root_patterns, { path = path, upward = true })[1]
-    root = root and vim.fs.dirname(root) or vim.uv.cwd()
-  end
-  return root
 end
 
 return M
