ceb68ea464fecde5026d9ff066a79344593c22c9
Frank <fbearoff@users.noreply.github.com>
2025-03-28 11:45:56 -0400

    consolidate autocmd

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -75,23 +75,15 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     "checkhealth",
     "help",
     "lspinfo",
+    "man",
     "notify",
     "qf",
-    "spectre_panel",
     "startuptime",
     "tsplayground",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
     vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-  end,
-})
-
--- make it easier to close man-files when opened inline
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "man" },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
   end,
 })
 
