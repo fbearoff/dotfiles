c83bff9620c24ee49891542ace542f699e31051f
Frank <fbearoff@users.noreply.github.com>
2025-03-28 11:10:44 -0400

    use opts table for kanagawa

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -6,57 +6,57 @@ return {
     "rebelot/kanagawa.nvim",
     event = "VimEnter",
     priority = 1000,
-    config = function()
-      require("kanagawa").setup({
-        undercurl = true, -- enable undercurls
-        commentStyle = { italic = false },
-        functionStyle = {},
-        keywordStyle = { italic = true },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = false, -- do not set background color
-        dimInactive = true, -- dim inactive window `:h hl-NormalNC`
-        terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = { -- add/modify theme and palette colors
-          palette = {},
-          theme = {
-            wave = {},
-            lotus = {},
-            dragon = {},
-            all = {
-              ui = { bg_gutter = "none" },
-            },
+    opts = {
+      undercurl = true, -- enable undercurls
+      commentStyle = { italic = false },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false, -- do not set background color
+      dimInactive = true, -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      colors = { -- add/modify theme and palette colors
+        palette = {},
+        theme = {
+          wave = {},
+          lotus = {},
+          dragon = {},
+          all = {
+            ui = { bg_gutter = "none" },
           },
         },
-        overrides = function(colors)
-          local theme = colors.theme
-          return {
-            -- for Snacks words
-            LspReferenceText = { bg = "none", underline = true },
-            LspReferenceWrite = { bg = "none", underline = true },
+      },
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          -- for Snacks words
+          LspReferenceText = { bg = "none", underline = true },
+          LspReferenceWrite = { bg = "none", underline = true },
 
-            -- Popup menu
-            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
-            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-            PmenuSbar = { bg = theme.ui.bg_m1 },
-            PmenuThumb = { bg = theme.ui.bg_p2 },
+          -- Popup menu
+          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+          PmenuSbar = { bg = theme.ui.bg_m1 },
+          PmenuThumb = { bg = theme.ui.bg_p2 },
 
-            -- nvim-ts-rainbow2
-            RainbowDelimiterRed = { fg = colors.palette.autumnRed },
-            RainbowDelimiterYellow = { fg = colors.palette.carpYellow },
-            RainbowDelimiterBlue = { fg = colors.palette.dragonBlue },
-            RainbowDelimiterOrange = { fg = colors.palette.surimiOrange },
-            RainbowDelimiterGreen = { fg = colors.palette.springGreen },
-            RainbowDelimiterViolet = { fg = colors.palette.oniViolet },
-            RainbowDelimiterCyan = { fg = colors.palette.waveAqua1 },
-          }
-        end,
-        theme = "wave", -- Load "wave" theme when 'background' option is not set
-        background = { -- map the value of 'background' option to a theme
-          dark = "wave", -- try "dragon" !
-          light = "lotus",
-        },
-      })
+          -- nvim-ts-rainbow2
+          RainbowDelimiterRed = { fg = colors.palette.autumnRed },
+          RainbowDelimiterYellow = { fg = colors.palette.carpYellow },
+          RainbowDelimiterBlue = { fg = colors.palette.dragonBlue },
+          RainbowDelimiterOrange = { fg = colors.palette.surimiOrange },
+          RainbowDelimiterGreen = { fg = colors.palette.springGreen },
+          RainbowDelimiterViolet = { fg = colors.palette.oniViolet },
+          RainbowDelimiterCyan = { fg = colors.palette.waveAqua1 },
+        }
+      end,
+      theme = "wave", -- Load "wave" theme when 'background' option is not set
+      background = { -- map the value of 'background' option to a theme
+        dark = "wave", -- try "dragon" !
+        light = "lotus",
+      },
+    },
+    init = function()
       require("kanagawa").load()
     end,
   },
