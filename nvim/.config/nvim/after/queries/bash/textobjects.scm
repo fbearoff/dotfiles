0c2168415ef3db04d5ba0297cd1ab55e7f1aadb3
Frank <fbearoff@users.noreply.github.com>
2026-04-16 13:45:47 -0400

    remove treesitter textobject queries, make-range doesn't work on main

--- nvim/.config/nvim/after/queries/bash/textobjects.scm
+++
@@ -1,9 +0,0 @@
-; extends
-
-; command
-(command) @command.inner
-
-(pipeline
-  "|" @_start
-  (command) @command.inner
-(#make-range! "command.outer" @_start @command.inner))
--- nvim/.config/nvim/after/queries/r/textobjects.scm
+++
@@ -1,7 +0,0 @@
-; extends
-;native pipe
-((binary_operator
-    lhs: (_)
-    "|>" @_start
-    rhs: (_) @pipe.inner)
-(#make-range! "pipe.outer" @_start @pipe.inner))
