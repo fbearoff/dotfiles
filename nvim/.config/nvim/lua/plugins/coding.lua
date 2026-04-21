d8e6d88490fbfb494569d667c35e2baf11ef4dde
Frank <fbearoff@users.noreply.github.com>
2026-04-21 12:35:14 -0400

    simplify surround

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -1,4 +1,21 @@
 return {
+  -- surround
+  {
+    "kylechui/nvim-surround",
+    keys = {
+      { mode = "i", "<C-g>s", "<Plug>(nvim-surround-insert)", desc = "Surround" },
+      { mode = "i", "<C-g>S", "<Plug>(nvim-surround-insert-line)", desc = "Surround Line" },
+      { "ys", "<Plug>(nvim-surround-normal)", desc = "Surround" },
+      { "yss", "<Plug>(nvim-surround-normal-cur)", desc = "Surround Current Line" },
+      { "yS", "<Plug>(nvim-surround-normal-line)", desc = "Surround Around Motion on New Lines" },
+      { "ySS", "<Plug>(nvim-surround-normal-cur-line)", desc = "Surround Around Current Line" },
+      { mode = "x", "S", "<Plug>(nvim-surround-visual)", desc = "Surround" },
+      { mode = "x", "gS", "<Plug>(nvim-surround-visual-line)", desc = "Surround Line" },
+      { "ds", "<Plug>(nvim-surround-delete)", desc = "Delete" },
+      { "cs", "<Plug>(nvim-surround-change)", desc = "Change" },
+      { "cS", "<Plug>(nvim-surround-change-line)", desc = "Change Line" },
+    },
+  },
 
   -- Code block joing/splitting
   {
