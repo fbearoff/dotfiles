d674d9d4ba9822c81b1a31b5640192e77e4b2682
Frank <fbearoff@users.noreply.github.com>
2026-05-07 11:25:31 -0400

    open blink cmdline menu only after 3 characters

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -60,6 +60,17 @@ return {
       },
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
+        providers = {
+          cmdline = {
+            min_keyword_length = function(ctx)
+              -- when typing a command, only show when the keyword is 3 characters or longer
+              if ctx.mode == "cmdline" and string.find(ctx.line, " ") == nil then
+                return 3
+              end
+              return 0
+            end,
+          },
+        },
       },
     },
   },
