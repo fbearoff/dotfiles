36800dd183fc5cc39b32d30be61296909c2e1c39
Frank <fbearoff@users.noreply.github.com>
2026-05-07 10:40:18 -0400

    move options to verbose format

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,60 +1,61 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-local options = {
-  backup = false, -- creates a backup file
-  breakindent = true, -- preserve indent on wrapped lines
-  clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-  cmdheight = 0, -- more space in the neovim command line for displaying messages
-  completeopt = { "menu", "menuone", "noselect" }, -- mostly just for cmp
-  confirm = true,
-  cursorline = true, -- highlight the current line
-  expandtab = true, -- convert tabs to spaces
-  fillchars = { foldopen = "", foldclose = "", fold = " ", foldsep = " ", diff = "╱", eob = " " },
-  foldcolumn = "1",
-  foldlevel = 99,
-  foldlevelstart = 99,
-  foldnestmax = 10, -- max fold nesting level
-  ignorecase = true, -- ignore case in search patterns
-  laststatus = 0, -- not needed with lualine
-  linebreak = true, -- break lines at delimiter chars
-  mouse = "", -- default value of "a", "nv" disables in insert
-  mousemodel = "extend",
-  number = true, -- set numbered lines
-  numberwidth = 2, -- set number column width to 2 {default 4}
-  pumheight = 10, -- pop up menu height
-  relativenumber = true, -- set relative numbered lines
-  ruler = false,
-  scrolloff = 8, -- lines of ceontext
-  shiftround = true, -- round indent
-  shiftwidth = 2, -- the number of spaces inserted for each indentation
-  showmatch = true, -- show matching paren on creation
-  showmode = false, -- we don't need to see things like -- INSERT -- anymore
-  showtabline = 0, -- always show tabs
-  sidescrolloff = 8,
-  signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
-  smartcase = true, -- smart case
-  smartindent = true, -- make indenting smarter again
-  smoothscroll = true,
-  softtabstop = 2, -- how many spaces is a tab press worth
-  splitbelow = true, -- force all horizontal splits to go below current window
-  splitkeep = "screen", -- stabilize screen position on split
-  splitright = true, -- force all vertical splits to go to the right of current window
-  swapfile = false, -- creates a swapfile
-  tabstop = 2, -- insert 2 spaces for a tab
-  termguicolors = true, -- set term gui colors (most terminals support this)
-  timeoutlen = 500, -- time to wait for a mapped sequence to complete (in milliseconds)
-  undofile = true, -- enable persistent undo
-  updatetime = 300, -- faster completion (4000ms default)
-  whichwrap = "bs<>[]hl", -- which "horizontal" keys are allowed to travel to prev/next line
-  wildmode = "longest:full,full", -- Command-line completion mode
-  writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.o.backup = false -- creates a backup file
+vim.o.breakindent = true -- preserve indent on wrapped lines
+vim.o.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+vim.o.cmdheight = 0 -- more space in the neovim command line for displaying messages
+vim.o.confirm = true
+vim.o.cursorline = true -- highlight the current line
+vim.o.expandtab = true -- convert tabs to spaces
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldnestmax = 10 -- max fold nesting level
+vim.o.ignorecase = true -- ignore case in search patterns
+vim.o.laststatus = 0 -- not needed with lualine
+vim.o.linebreak = true -- break lines at delimiter chars
+vim.o.mouse = "" -- default value of "a", "nv" disables in insert
+vim.o.mousemodel = "extend"
+vim.o.number = true -- set numbered lines
+vim.o.numberwidth = 2 -- set number column width to 2 {default 4}
+vim.o.pumheight = 10 -- pop up menu height
+vim.o.relativenumber = true -- set relative numbered lines
+vim.o.ruler = false
+vim.o.scrolloff = 8 -- lines of ceontext
+vim.o.shiftround = true -- round indent
+vim.o.shiftwidth = 2 -- the number of spaces inserted for each indentation
+vim.o.showmatch = true -- show matching paren on creation
+vim.o.showmode = false -- shown on statusline
+vim.o.showtabline = 0 -- always show tabs
+vim.o.sidescrolloff = 8
+vim.o.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
+vim.o.smartcase = true -- smart case
+vim.o.smartindent = true -- make indenting smarter again
+vim.o.smoothscroll = true
+vim.o.softtabstop = 2 -- how many spaces is a tab press worth
+vim.o.splitbelow = true -- force all horizontal splits to go below current window
+vim.o.splitkeep = "screen" -- stabilize screen position on split
+vim.o.splitright = true -- force all vertical splits to go to the right of current window
+vim.o.swapfile = false -- creates a swapfile
+vim.o.tabstop = 2 -- insert 2 spaces for a tab
+vim.o.termguicolors = true -- set term gui colors (most terminals support this)
+vim.o.timeoutlen = 500 -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.o.undofile = true -- enable persistent undo
+vim.o.updatetime = 300 -- faster completion (4000ms default)
+vim.o.whichwrap = "bs<>[]hl" -- which "horizontal" keys are allowed to travel to prev/next line
+vim.o.wildmode = "longest:full,full" -- Command-line completion mode
+vim.o.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+
+vim.opt.fillchars = {
+  diff = "╱",
+  eob = " ",
+  fold = " ",
+  foldclose = "",
+  foldopen = "",
+  foldsep = " ",
 }
-
-for k, v in pairs(options) do
-  vim.opt[k] = v
-end
-
+vim.opt.completeopt = { "menu", "menuone", "noselect" } -- mostly just for cmp
 vim.opt.shortmess:append({ W = true, I = true, c = true })
 vim.opt.iskeyword:append("-") -- hyphenated words recognized by searches
 vim.opt.formatoptions:remove({ "c", "r", "o" }) -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
