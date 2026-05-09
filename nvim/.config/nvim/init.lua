6a5d89fb13e2208622baca3530913051ff45c36e
Frank <fbearoff@users.noreply.github.com>
2026-05-09 19:30:30 -0400

    disable builtin plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,28 @@
 vim.loader.enable()
+
+-- disable builtin plugins
+vim.g.loaded_2html_plugin = true
+vim.g.loaded_getscript = true
+vim.g.loaded_getscriptPlugin = true
+vim.g.loaded_gzip = true
+vim.g.loaded_logipat = true
+vim.g.loaded_matchit = true
+vim.g.loaded_matchparen = true
+vim.g.loaded_netrw = true
+vim.g.loaded_netrwFileHandlers = true
+vim.g.loaded_netrwPlugin = true
+vim.g.loaded_netrwSettings = true
+vim.g.loaded_spellfile_plugin = true
+vim.g.loaded_tar = true
+vim.g.loaded_tarPlugin = true
+vim.g.loaded_tohtml = true
+vim.g.loaded_tutor = true
+vim.g.loaded_tutor_mode_plugin = true
+vim.g.loaded_vimball = true
+vim.g.loaded_vimballPlugin = true
+vim.g.loaded_zip = true
+vim.g.loaded_zipPlugin = true
+
 require("config.options")
 require("config.autocmds")
 require("config.keymaps")
