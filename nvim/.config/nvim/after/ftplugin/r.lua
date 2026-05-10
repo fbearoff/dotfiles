5a23b5e2b797af24a1fcda97ab676bfa593e1c28
Frank <fbearoff@users.noreply.github.com>
2026-05-10 19:20:05 -0400

    make ftplugins use buffer local options

--- nvim/.config/nvim/after/ftplugin/r.lua
+++ nvim/.config/nvim/after/ftplugin/r.lua
@@ -1,5 +1,5 @@
 -- keymaps
-vim.keymap.set("i", ">>", "|> ", { desc = "Insert Pipe (|>)" })
-vim.keymap.set("i", "<<", "<- ", { desc = "Insert Left Assign (<-)" })
-vim.keymap.set("i", "i>", "%in% ", { desc = "Insert %in%" })
-vim.keymap.set("i", "n>", "%notin% ", { desc = "Insert %notin%" })
+vim.keymap.set("i", ">>", "|> ", { desc = "Insert Pipe (|>)", buf = 0 })
+vim.keymap.set("i", "<<", "<- ", { desc = "Insert Left Assign (<-)", buf = 0 })
+vim.keymap.set("i", "i>", "%in% ", { desc = "Insert %in%", buf = 0 })
+vim.keymap.set("i", "n>", "%notin% ", { desc = "Insert %notin%", buf = 0 })
