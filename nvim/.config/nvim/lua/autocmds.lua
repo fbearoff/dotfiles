a92374b60db44f5061ccdf47df9f617b1b3d0949
Frank <fbearoff@users.noreply.github.com>
2026-05-15 12:31:30 -0400

    remove cursorline autocmd, was source of picker crashing

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -16,22 +16,6 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
   end,
 })
 
--- show cursorline only in active window enable
-vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
-  group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
-  callback = function()
-    vim.opt_local.cursorline = true
-  end,
-})
-
--- show cursorline only in active window disable
-vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
-  group = "active_cursorline",
-  callback = function()
-    vim.opt_local.cursorline = false
-  end,
-})
-
 -- Create directories when needed, when saving a file
 vim.api.nvim_create_autocmd("BufWritePre", {
   group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
